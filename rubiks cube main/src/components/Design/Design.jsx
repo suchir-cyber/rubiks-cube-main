f823b3910c63c3f057809c27c57b08a5244d0ede
suchir_18 <97435643+suchir-cyber@users.noreply.github.com>
2023-07-23 15:02:17 +0530

    Add files via upload

---
+++ rubiks cube main/src/components/Design/Design.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Design.css";
+
+function TopDesign() {
+    return <div className="topDesign">
+        <div>
+        </div>
+    </div>
+}
+
+function BottomDesign() {
+    return <div className="bottomDesign">
+        <div>
+        </div>
+    </div>
+}
+
+export {TopDesign, BottomDesign};
